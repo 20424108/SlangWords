e26de1cdf0e65e66b3b3421bc7d6e24145dce65d
20424108 <20424108@student.hcmus.edu.vn>
2021-04-25 18:50:18 +0700

    Create Slang.java

---
+++ src/com/tranphuongnam/models/Slang.java
@@ -0,0 +1,11 @@
+package com.tranphuongnam.models;
+
+public class Slang {
+    protected String slang;
+    protected String meaning;
+    public Slang(){}
+    public Slang(String slang, String meaning) {
+        this.slang = slang;
+        this.meaning = meaning;
+    }
+}
